af5660b84dbb4a9198fc4fcce0bc723549f2ce13
Danielle Hiebert <danielle@aruaki.ca>
2026-01-27 13:50:47 -0700

    feat: html autotag

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup() -- Enable auto tag closing
+    end,
+    ft = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'vue', 'svelte' }, -- Activate for relevant filetypes
+    lazy = true, -- Lazy-load the plugin when required
+  },
+}
