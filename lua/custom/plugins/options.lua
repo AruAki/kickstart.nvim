12e48fcb134f03870b0984192c7ecacf6db6bfba
Danielle Hiebert <danielle@aruaki.ca>
2026-05-14 22:53:46 -0600

    don't copy to clipboard on "d" and "x"

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -1,10 +1,16 @@
--- Write to same file, otherwise Bun hotreload crashes
+-- NOTE: Write to same file, otherwise Bun hotreload crashes
 vim.o.backupcopy = 'yes'
 
--- Set tab size to 2 spaces
+-- NOTE: Set tab size to 2 spaces
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.o.relativenumber = true
 
+-- NOTE: Delete into the black hole register so it doesn't overwrite clipboard
+vim.keymap.set({ 'n', 'v' }, 'd', '"_d')
+vim.keymap.set({ 'n', 'v' }, 'dd', '"_dd')
+vim.keymap.set({ 'n', 'v' }, 'D', '"_D')
+-- NOTE: Do the same for 'x' (delete single character)
+vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
