524162b8c1d04b4cd98d9f139309907b880d4301
Danielle Hiebert <danielle@aruaki.ca>
2026-05-15 09:19:54 -0600

    fix: svelte not highlighting properly

--- init.lua
+++ init.lua
@@ -904,7 +904,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'svelte', 'typescript' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'svelte', 'typescript', 'javascript', 'css' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
