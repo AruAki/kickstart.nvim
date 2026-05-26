e2be38534603f84d24ee4f26d69c4150e9b5480c
Danielle Hiebert <danielle@aruaki.ca>
2026-05-26 11:22:54 -0600

    disable black hole register

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -9,8 +9,8 @@ vim.o.expandtab = true
 vim.o.relativenumber = true
 
 -- NOTE: Delete into the black hole register so it doesn't overwrite clipboard
-vim.keymap.set({ 'n', 'v' }, 'd', '"_d')
-vim.keymap.set({ 'n', 'v' }, 'dd', '"_dd')
-vim.keymap.set({ 'n', 'v' }, 'D', '"_D')
+-- vim.keymap.set({ 'n', 'v' }, 'd', '"_d')
+-- vim.keymap.set({ 'n', 'v' }, 'dd', '"_dd')
+-- vim.keymap.set({ 'n', 'v' }, 'D', '"_D')
 -- NOTE: Do the same for 'x' (delete single character)
-vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
+-- vim.keymap.set({ 'n', 'v' }, 'x', '"_x')
