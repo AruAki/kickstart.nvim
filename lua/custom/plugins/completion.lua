7f24764003d7f589adf4769d546464e00da2aa4f
Danielle Hiebert <danielle@aruaki.ca>
2026-06-20 19:38:03 -0600

    fix: llm being bad

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -3,17 +3,17 @@ vim.pack.add {
   'https://github.com/rafamadriz/friendly-snippets',
 }
 
-local luasnip = require('luasnip')
+local luasnip = require 'luasnip'
 
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({ 
-  paths = { vim.fn.stdpath("config") .. "/snippets" } 
-})
+require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load {
+  paths = { vim.fn.stdpath 'config' .. '/snippets' },
+}
 -- Prefer custom ones over default
-luasnip.config.set_config({
+luasnip.config.set_config {
   history = true,
   override_priority = 1000,
-})
+}
 
 require('blink.cmp').setup {
   keymap = { preset = 'default' },
@@ -32,35 +32,30 @@ require('blink.cmp').setup {
   },
 }
 
--- 3. Modern LSP Setup
-local capabilities = require('blink.cmp').get_lsp_capabilities()
+-- local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-vim.lsp.enable('svelte', {
-  capabilities = capabilities,
+vim.lsp.config('svelte', {
+  -- capabilities = capabilities,
+  cmd_env = {
+    CHOKIDAR_IGNORE = '/home/danielle/.var',
+  },
   settings = {
     svelte = {
       plugin = {
         svelte = {
-          -- This is the magic toggle to show cleaner documentation
-          hover = { enable = true }
+          hover = { enable = true },
         },
-        -- If you hate the internal TS definitions, you can tweak these
         typescript = { hover = { enable = true } },
-      }
-    }
+      },
+    },
   },
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      pattern = { "*.js", "*.ts" },
-      callback = function(ctx)
-        client.notify("$/onDidChangeTsOrJsFile", { uri = vim.uri_from_bufnr(ctx.buf) })
-      end,
-    })
-  end,
+  -- on_attach = function(client, bufnr)
+  --   vim.api.nvim_create_autocmd('BufWritePost', {
+  --     pattern = { '*.js', '*.ts' },
+  --     callback = function(ctx) client.notify('$/onDidChangeTsOrJsFile', { uri = vim.uri_from_bufnr(ctx.buf) }) end,
+  --   })
+  -- end,
 })
 
--- Other servers
 local web_servers = { 'ts_ls', 'cssls', 'html', 'tailwindcss', 'svelte' }
-for _, server in ipairs(web_servers) do
-  vim.lsp.enable(server, { capabilities = capabilities })
-end
+vim.lsp.enable(web_servers)
